184c4c6f494c6090de7719baa5a16ca0c694aeff
RMartires <rohitmartires14@gmail.com>
2021-03-14 15:40:05 +0530

    mobile responsive

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-(function($){
+(function ($) {
   // variables
-  elementWidth = $('ul').width(),
-  containerWidth = $('nav').width(),
-  difference = elementWidth-containerWidth,
-  finalWidth = difference * 1.5,
-  element = $('ul');
-  
+  (elementWidth = $('ul').width()),
+    (containerWidth = $('nav').width()),
+    (difference = elementWidth - containerWidth),
+    (finalWidth = difference * 1.5),
+    (element = $('ul'));
+
   // active on click
-  $('li').on('click', function(){
+  $('li').on('click', function () {
     $('li').removeClass('active');
     $(this).addClass('active');
   });
-  
 })(jQuery);
+
+// document.getElementById('home').addEventListener('click', function () {
+//   alert('Hello World!');
+// });
